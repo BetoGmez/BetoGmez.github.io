01aea9430cee86e63f30fca401dbfd723dcdba79
Beto Gomez <luisrg1402@gmail.com>
2024-08-15 15:38:20 -0600

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+document.getElementById("contactButton").addEventListener("click", function() {
+    window.location.href = "mailto:luisrg1402@gmail.com?subject=Contacto%20Profesional&body=Hola%20Alberto%20Gómez,%0A%0AEstoy%20interesado%20en%20contactarte%20por%20motivos%20profesionales.%0A%0A¡Gracias!";
+});
